9c86a260d729925d406a25b91174ed7ec85c3cb7
Ablazed <ablazedsyntax@gmail.com>
2016-06-26 14:31:28 +0200

    Update Ablazed.js

--- Ablazed.js
+++ Ablazed.js
@@ -1,4 +1,4 @@
-var Ablazed = (function(exports) {
+var AblazedExt = (function(exports) {
   exports.LoadStyle = function(url, async) {
     async = async || true;
     var link = document.createElement('link'),
@@ -27,5 +27,23 @@ var Ablazed = (function(exports) {
     script.src = url;
     ref.appendChild(script);
   };
+  exports.Paint = function() {
+    var query = document.querySelectorAll('script[src*="Ablazed.js?"]');
+    if (query.length > 0) {
+      var regex = new RegExp(/([\w]+)$/gm),
+        url = query[0].src,
+        paint = url.match(regex)[0];
+      exports.LoadStyle('https://ablazed.github.io/Paint/' + paint.toLowerCase() + '.css', true);
+    } else {
+      exports.LoadStyle('https://ablazed.github.io/Paint/default.css', true);
+    }
+  };
   return exports;
 })({});
+
+(function() {
+  window.onload = function() {
+    AblazedExt.Paint();
+    AblazedExt.Lang();
+  };
+})();
